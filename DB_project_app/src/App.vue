264587534479e977e2be369ae959c0beedf34be3
Nahomy <namyjimenez23@gmail.com>
2025-06-07 22:34:15 -0600

    style

--- DB_project_app/src/App.vue
+++ DB_project_app/src/App.vue
@@ -8,21 +8,66 @@
 
     
       <div class="navbar-nav ms-auto">
+
         <router-link to="/users" class="nav-link" active-class="active">
           <i class="bi bi-people me-1"></i>Usuarios
         </router-link>
-        <router-link to="/reservas" class="nav-link" active-class="active">
-          <i class="bi bi-calendar-check me-1"></i>Reservas
-        </router-link>
-        <router-link to="/tours" class="nav-link" active-class="active">
-          <i class="bi bi-map me-1"></i>Tours disponibles
-        </router-link>
-        <router-link to="/extras" class="nav-link" active-class="active">
-          <i class="bi bi-gift me-1"></i>Extras ofrecidos
-        </router-link>
+
+        <div class="nav-item dropdown">
+          <a
+          class="nav-link dropdown-toggle"
+          href="#"
+          role="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+          >
+          <i class="bi bi-calendar-check-me"></i>Reservas
+          </a>
+
+          <ul class="dropdown-menu dropdown-menu-dark">
+             <li><router-link to="/reservas" class="dropdown-item">Listar</router-link></li>
+             <li><router-link to="/reservas/agregar" class="dropdown-item">Agregar</router-link></li>
+          </ul>
+
+        </div>
+
+       
+       <div class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <i class="bi bi-map me-1"></i>Tours disponibles
+          </a>
+          <ul class="dropdown-menu dropdown-menu-dark">
+            <li><router-link to="/tours" class="dropdown-item">Listar</router-link></li>
+            <li><router-link to="/tours/agregar" class="dropdown-item">Agregar</router-link></li>
+          </ul>
+        </div>
+
+       <div class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <i class="bi bi-gift me-1"></i>Extras ofrecidos
+          </a>
+          <ul class="dropdown-menu dropdown-menu-dark">
+            <li><router-link to="/extras" class="dropdown-item">Listar</router-link></li>
+            <li><router-link to="/extras/agregar" class="dropdown-item">Agregar</router-link></li>
+          </ul>
+        </div>
+
         <router-link to="/contacto" class="nav-link" active-class="active">
           <i class="bi bi-envelope me-1"></i>Contáctenos
         </router-link>
+
       </div>
 
     </div>
