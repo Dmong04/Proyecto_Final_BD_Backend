d625638053765175a19606fda763104a9b17dc71
Pinaruma <juan98xxxx98@gmail.com>
2025-08-14 20:05:10 -0600

    Procedimiento_coco_tours_cliente.sql

--- DB_PROJECT_DATABASE/Procedimiento_coco_tours_cliente.sql
+++ DB_PROJECT_DATABASE/Procedimiento_coco_tours_cliente.sql
@@ -9,7 +9,45 @@ create procedure pa_client_users (
 	@password VARCHAR(150) = null
 ) as
 begin
+begin try
+	if @name = null or @name = ' '
+	begin
+		raiserror('El nombre no corresponde a un valor válido', 16, 1)
+		return
+	end
 	insert into client([name]) values (@name)
+
+	if @phone like '%[^0-9]%' or len(@phone) <> 8
+		begin
+			raiserror('El formato del teléfono no es válido', 16, 1)
+			return
+		end
+	else if exists (select 1 from [client_phones] where phone = @phone)
+		begin
+			raiserror('El teléfono no corresponde a un valor válido', 16, 1)
+			return
+		end
+
+	if @email not like '%_@_%._%'
+		begin
+			raiserror('El formato email no es válido', 16, 1)
+			return
+		end
+	else if exists (select 1 from [user] where email = @email)
+		begin
+			raiserror('El correo no corresponde a un valor válido', 16, 1)
+			return
+		end
+	if exists (select 1 from [user] where username = @username)
+		begin
+			raiserror('El nombre de usuario no es válido', 16, 1)
+			return
+		end
+	if @password = null or @password = ' '
+		begin
+			raiserror('La contraseña no es válida', 16, 1)
+			return
+		end
 	DECLARE @client_phone_id INT;
 	DECLARE @client_id INT;
 	SELECT @client_id = @@IDENTITY;
@@ -17,10 +55,15 @@ begin
 	insert into client_phones(client_id, [phone]) values (@client_phone_id, @phone)
 	insert into [user] (email, username, [password], client_id, admin_id)
 	values (@email, @username, @password, @client_id, null)
+end try
+begin catch
+	raiserror('Hubo un error en el proceso, por favor, revise los datos ingresados', 16, 1)
+	return
+end catch
 end
 go
 
-execute pa_client_users 'client','80808080', 'client2@gmail.com', 'client2', '12345678'
+execute pa_client_users 'clientillouo','80808088', 'client2656@gmail.com', 'client2656', '12345678'
 
 select * from [user]
 
