d18819524a70c88ef1e5979da5057e1fd84bdf69
Dereck Monge Chaves <168316345+Dmong04@users.noreply.github.com>
2025-08-21 19:53:43 -0600

    remove files from backend project

--- DB_project_app/src/models/phone_client.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from "./client";
-
-export class PhoneClient {
-    constructor(
-        public id: number,
-        public phone: string,
-        public client: Client
-    ) { }
-}
--- DB_project_app/src/models/provider.ts
+++
@@ -1,8 +0,0 @@
-export class Provider {
-    constructor(
-        public id: number,
-        public name: string,
-        public description: string,
-        public email: string
-    ) { }
-}
