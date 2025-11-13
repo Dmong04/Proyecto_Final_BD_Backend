fdecb547d0f49f98970f9b7bf32f9b021551a3ed
cLamberti <150824061+cLamberti@users.noreply.github.com>
2025-11-13 03:44:23 -0600

    Update APIRoutes.java

--- DB_Project/src/main/java/com/una/security/token/routes/APIRoutes.java
+++ DB_Project/src/main/java/com/una/security/token/routes/APIRoutes.java
@@ -10,10 +10,8 @@ public class APIRoutes {
     
     public final List<String> clientRoutes = List.of(
             "/coco_tours/api/v2/client/**",
-            "/coco_tours/api/v2/extra/all",
             "/coco_tours/api/v2/passengers/**",
             "/coco_tours/api/v2/reservation/**",
-            "/coco_tours/api/v2/tours/all",
             "/coco_tours/api/v2/user/**"
     );
     
